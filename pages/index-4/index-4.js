8082859c7b24f7203ed130d67e9c8124e9df4b84
cairuikai <542163616@qq.com>
2022-03-30 16:26:59 +0800

    yueqiu

---
+++ pages/index-4/index-4.js
@@ -0,0 +1,88 @@
+// pages/index-4/index-4.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    time: 'xxxx',     //获取时间
+    court: 'xxxx',    //获取球场
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  // onShareAppMessage: function () {
+
+  // },
+
+  // 头像获取
+  // onHeadPortrait: function(e){
+
+  // },
+
+
+  
+  // 添加按钮 (点击添加时搜索头像匹配)
+  onAddition: function(e){
+
+  },
+
+  //球赛预约按钮
+  onBallGameReservation: function(e){
+
+  },
+
+
+
+
+})
---
+++ pages/index10/index.js
@@ -0,0 +1,44 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+
+  data:{
+    msg: 'Hellso World',
+  },
+
+  clickMe: function(){
+    this.setData({msg: "s"})
+  },
+
+
+
+//自选
+  //单人匹配
+  onSinglePersonMatch: function(e){
+
+  },
+  //多人匹配
+  onMultiPersonMatch: function(e){
+
+  },
+  //球赛预约
+  onBallGameReservation: function(e){
+
+  },
+  //3v3
+  onThree: function(e){
+
+  },
+  //4v4
+  onFour: function(e){
+
+  },
+  //5v5
+  onFive: function(e){
+
+  },
+
+
+})
